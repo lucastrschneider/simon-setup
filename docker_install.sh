a134c52b42a7cf7c817daa33dfe591cd8d1b0cc6
lucastrschneider <lucastrschneider@gmail.com>
2022-01-13 11:25:09 -0300

    ✨ Add rocker install

--- docker_install.sh
+++ docker_install.sh
@@ -23,6 +23,9 @@ sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Rocker installation
+sudo apt install python3-rocker
+
 # Docker post-installation
 sudo groupadd docker
 sudo usermod -aG docker $USER
