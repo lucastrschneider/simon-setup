f20363a4f82c82607d0cf2a84d06869e5f274180
lucastrschneider <lucastrschneider@gmail.com>
2022-10-30 01:48:26 +0200

    Add vscode installation

--- basic_install.sh
+++ basic_install.sh
@@ -28,6 +28,22 @@ git config --global user.email "$GITHUB_EMAIL"
 # Install alacritty
 sudo dnf install alacritty -y
 
+# Install vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo dnf check-update
+
+sudo dnf install code -y
+
 # Install fish and related
 sudo dnf install fish util-linux-user -y
 
