72ef137fe797a6ffe97f82dc8b3e43ee3c3a74ba
lucastrschneider <lucastrschneider@gmail.com>
2022-10-30 02:19:22 +0200

    Update docker install for fedora

--- docker_install.sh
+++ docker_install.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
 # Docker installation
-sudo apt update
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
 
-sudo apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg \
-     lsb-release -y
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-
-# Docker compose installation
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl start docker
 
 # Rocker installation
-sudo apt install python3-rocker
+sudo pip3 install rocker
 
 # Docker post-installation
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Start docker on boot
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
