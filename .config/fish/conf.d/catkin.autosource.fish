8cf06aabd4c6281b5f9d28f50437dbbc05acb80b
lucastrschneider <lucastrschneider@gmail.com>
2023-01-05 14:07:39 +0100

    🧑‍💻 Update fish configuration - Remove unused ROS stuff

--- .config/fish/conf.d/catkin.autosource.fish
+++
@@ -1,7 +0,0 @@
-function catkinSource --on-variable PWD
-    status --is-command-substitution; and return
-    if test -e ".catkin_workspace"; or test -e ".catkin_tools"
-        bass source devel/setup.bash
-        echo "Configured the folder as a workspace"
-    end
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 alias cube='/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX/STM32CubeMX'
 
+alias update_grub='sudo grub2-mkconfig -o /etc/grub2-efi.cfg'
+
 source ~/.config/fish/theme.fish
 
 set -x CUBE_PATH             	/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX
@@ -10,6 +12,3 @@ set -x GHDL_PATH	      		/usr/local/ghdl/bin
 set     PATH $PATH $ARM_GCC_PATH
 set     PATH $PATH $STM32_CUBE_PROGRAMMER
 set     PATH $PATH $GHDL_PATH
-
-# bass source /opt/ros/noetic/setup.bash
-# bass source ~/bt_ws/devel/setup.bash
