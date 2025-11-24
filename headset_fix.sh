8e3009c07bb3852e2232a984ffacca4ef4c94a37
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2025-11-24 19:14:38 -0300

    Update scripts

--- headset_fix.sh
+++ headset_fix.sh
@@ -3,5 +3,10 @@ ALSA_CONFIG_FILE="/etc/modprobe.d/alsa-base.conf"
 original_file="${ALSA_CONFIG_FILE}"
 backup_file="${ALSA_CONFIG_FILE}.bak"
 
-cp "$original_file" "$backup_file"
-echo "options snd-hda-intel model=headset-mic,dell-headset-multi" >> "$original_file"
+if [ -f "$original_file" ]; then
+    sudo cp "$original_file" "$backup_file"
+    sudo echo "options snd-hda-intel model=headset-mic,dell-headset-multi" >> "$original_file"
+else
+    sudo touch "$original_file"
+    sudo echo "options snd-hda-intel model=headset-mic,dell-headset-multi" >> "$original_file"
+fi
